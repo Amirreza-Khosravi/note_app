de0f80dec32c3b5505746ce751b0417ef5c8f550
Amirreza <imamirreza.kh@gmail.com>
2023-07-23 01:09:01 +0330

    edit file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/data/model/task.dart';
 import 'package:note_app/data/model/task_type.dart';
 import 'package:note_app/data/model/type_enum.dart';
-import 'package:note_app/screens/splash_screen.dart';
+import 'package:note_app/screens/home_screen.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -22,6 +24,49 @@ class Application extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Splashscreen(),
+    );
+  }
+}
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: ((context) => HomeScreen())));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SpinKitRipple(
+            color: Color(0xff18DAA3),
+            size: 100,
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Task Note',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
